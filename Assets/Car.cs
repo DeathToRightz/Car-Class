fdc0cb5ad72e41a2c9cde52fdc8ccddc64a8f75a
DeathToRightz <logan.cordova@gmail.com>
2024-02-15 13:07:46 -0500

    added Comments

--- Assets/Car.cs
+++ Assets/Car.cs
@@ -1,54 +1,52 @@
-
-
-
-
-public class Car 
+//////////////////////////////////////////////
+//Assignment/Lab/Project: Car Class
+//Name: Logan Cordova
+//Section: SGD.213.2172
+//Instructor: Professor Sowers
+//Date: 02/15/2024
+/////////////////////////////////////////////
+public class Car
 {
-   
-       private int _year;
-       private string _make;
-       private int _maxSpeed = 100;
-       private int _currentSpeed = 0;
-      
-        public Car(int year, string make) 
+    //The needed private variables
+    private int _year;
+    private string _make;
+    private int _maxSpeed = 100;
+    private int _currentSpeed = 0;
+
+    //The car Instance
+    public Car(int year, string make)
+    {
+        Year = year;
+        Make = make;
+    }
+    // Public property that gets the year of the car
+    public int Year
+    {
+        get
         {
-            /*_year = year;
-            _make = make;*/
-            Year = year;
-            Make = make;
-         
-        }  
-        public int Year
+            return _year;
+
+        }
+        set
         {
-            get
+            //If the year of the car falls below or above the year range it will force it to be the minumum or maximum range.
+            if (value >= 1886 && value <= 2024)
             {
-                return _year;
-                
+                _year = value;
             }
-            set
-            {
-                if (value >= 1886 && value <=2024 )
-                {
-                   
-                     _year = value;
-                }
             else if (value < 1886)
             {
-
                 value = 1886;
                 _year = value;
-
             }
-             else if(value > 2024)
+            else if (value > 2024)
             {
                 value = 2024;
                 _year = value;
-
-            }
-           
-                
             }
         }
+    }
+    //Public property that gets make of the car
     public string Make
     {
         get
@@ -57,33 +55,27 @@ public class Car
         }
         set
         {
-
-            if (value != "")
-            {
-                _make = value;
-            }
-            else
-            {
-                _make = "";
-            }
+            _make = value;
         }
     }
+    //Increases the speed of the car and will stop it before it reaches the max
     public int IncreaseSpeed()
+    {
+        if (_currentSpeed < _maxSpeed)
         {
-           if(_currentSpeed < _maxSpeed)
-            {
-                return _currentSpeed += 10;
-            }
-            return _currentSpeed;
+            return _currentSpeed += 10;
         }
-        public int DecreaseSpeed()
+        return _currentSpeed;
+    }
+    //Decreases the speed of the car and will stop it before it reaches below 0
+    public int DecreaseSpeed()
+    {
+        if (_currentSpeed > 0)
         {
-            if (_currentSpeed > 0)
-            {
-                return _currentSpeed -= 10;
-            }
-            return _currentSpeed;
+            return _currentSpeed -= 10;
         }
- 
-    
+        return _currentSpeed;
+    }
+
+
 }
--- Assets/TestCar.cs
+++ Assets/TestCar.cs
@@ -1,23 +1,33 @@
-using System.Collections;
-using System.Collections.Generic;
+//////////////////////////////////////////////
+//Assignment/Lab/Project: Car Class
+//Name: Logan Cordova
+//Section: SGD.213.2172
+//Instructor: Professor Sowers
+//Date: 02/15/2024
+/////////////////////////////////////////////
 using TMPro;
 using UnityEngine;
 //using static Car;
 using UnityEngine.SceneManagement;
 public class TestCar : MonoBehaviour
-{
+    
+{   //Text and Input Fields for car's year and make
     [SerializeField] TMP_InputField carYearInput;
     [SerializeField] TMP_Text carYearTxt;
     [SerializeField] TMP_Text carMakeTxt;
     [SerializeField] TMP_InputField carMakeInput;
     [SerializeField] TMP_Text speedText;
+
+    //Make reference to the canvases
     public GameObject yearCanvas,createCanvas,makeCanvas;
     
-   
+   //Creates reference Car class variable
     Car newCar;
     void Start()
     {
+        //Creates car variable for future use
         newCar = new Car(0000, "thing");
+        //Deactivate other canvases
         yearCanvas.SetActive(false);
         createCanvas.SetActive(false);
 
@@ -27,34 +37,33 @@ public class TestCar : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //If the player presses the up or down arrow it starts the function in the car Class and displays the change in speed
         if (Input.GetKeyDown(KeyCode.UpArrow))
         {
-            Debug.Log("up");
-            
-            
-           
+            Debug.Log("up");       
             speedText.text =$"Current Speed {newCar.IncreaseSpeed().ToString()}";
         }
         if (Input.GetKeyDown(KeyCode.DownArrow))
         {
-            Debug.Log("Down");
-           
+            Debug.Log("Down");          
            speedText.text = $"Current Speed {newCar.DecreaseSpeed().ToString()}";
         }
     }
+    //If the player doesn't input a name for the make the button wont activate
     public void OnClickGetMake()
     {
         if(carMakeInput.text == "")
         {
             return;
         }
-        
+        //If valid it makes the newCar variable the inputed make and sets the corrisponding panels false
         newCar.Make = carMakeInput.text;       
         makeCanvas.SetActive(false);
         yearCanvas.SetActive(true);
       
     }
+
+    //Gets the year inputed and sets it to the car variable for the year and sets the corrisponding panels false
     public void OnClickGetYear()
     {
      
@@ -64,7 +73,7 @@ public class TestCar : MonoBehaviour
 
     }
     
-
+    //Creates the car variable using the players inputed values
     public void OClickCreateCar()
     {
         carMakeTxt.text = newCar.Make;
